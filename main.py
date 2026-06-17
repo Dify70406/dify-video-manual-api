6020b3f77a618ca2db09e1bd7fcfe4b3ab2f25f8
Dify70406 <playborderside@gmail.com>
2026-06-17 14:13:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "OK"
+
 @app.route("/manual", methods=["POST"])
 def manual():
-    return {
+    return jsonify({
         "file_url": "https://example.com/test.docx"
-    }
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
